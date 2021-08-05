fd4326af452b6021d868324f1966cd1f95f69069
Caio lima <limacaio438@gmail.com>
2021-08-04 21:33:07 -0300

    Adicionando projeto

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá,mundo, projeto no visual studio");
+
+            Console.WriteLine("A execuçao acabou. Tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
